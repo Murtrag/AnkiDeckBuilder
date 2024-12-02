04521fbec1d02e8aa987a051d10780684e85e0f2
---- <lukasz.czernik@gmail.com>
2024-12-02 15:46:19 +0100

    Fixed loop

--- 1_deck_from_plain_text.py
+++ 1_deck_from_plain_text.py
@@ -20,26 +20,25 @@ deck_model = SimpleDeckModel().create_deck_model()
 # Create a new deck
 deck = SimpleDeck(2059417507, deck_name).create_deck()
 
+# Create notes
 with open(file_with_plain_text, 'r') as file:
-    words = file.read()
-
-words_lines = words.split('\n')
-for line in words_lines:
-    sleep(10)
-    try:
-        front, back = line.split(' : ')
-        front = front.lower()
-        back = back.lower()
-        # sound_url = f'https://diki.pl/images-common/en/mp3/{front}.mp3'
-
-    except Exception as e:
-        print(f'skipped: {e}')
-        continue
-    # Create note
-    note = SimpleNote().create_note(deck_model, DutchPodAudio, front, back)
-    deck.add_note(note)
+    for line in file:
+        # Time delay to not anger scraped websites
+        sleep(10) 
+        try:
+            front, back = line.strip().split(' : ')
+            front = front.lower()
+            back = back.lower()
+        except ValueError as e:
+            print(f"Skipped line due to error: {e} | Line content: {line.strip()}")
+            continue
+        
+        # Create note
+        note = SimpleNote().create_note(deck_model, DutchPodAudio, front, back)
+        deck.add_note(note)
 
 # Generate package
 package = SimplePackage().create_package(deck)
 
+# Save deck
 package.write_to_file(f'{deck_name}.apkg')
