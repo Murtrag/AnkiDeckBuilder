a4bdc8d4cd86e53b2167df912b95f3a0efc878df
---- <lukasz.czernik@gmail.com>
2024-12-12 16:49:05 +0100

    Update audio context

--- anki_gendeck_cli/strategies/context.py
+++ anki_gendeck_cli/strategies/context.py
@@ -1,20 +1,26 @@
+from os.path import join
+
 from ..utils.download import download_audio
 
 class AudioContext:
-    def __init__(self):
-        self.download = download_audio
+    def __init__(self, audio_dir="audio"):
         self.strategy = None
+        self.audio_dir = audio_dir
+        self.download = download_audio
 
-    def set_strategy(self, strategy: 'BaseAudio'):
+    def set_strategy(self, strategy: 'BaseAudio') -> None:
         self.strategy = strategy
 
-    def get_audio(self, file_name: str) -> None:
+    def get_audio(self, file_name: str) -> str:
         audio_obj = self.strategy(file_name)
-        # breakpoint()
+        
         self.download(
             audio_obj.get_audio_url(),
             file_name,
+            audio_dir=self.audio_dir,
             headers=audio_obj.headers
         )
+        return join(self,audio_dir, file_name)
+
 
     
